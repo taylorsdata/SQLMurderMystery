0dd73c053a223a0f98c5aabe5b66eaee115d0cd8
Tay <taylorhaynes21@gmail.com>
2025-04-21 10:11:11 -0500

    Update 01_intro_queries.sql

--- queries/01_intro_queries.sql
+++ queries/01_intro_queries.sql
@@ -1,4 +1,4 @@
--- 📝 Step 1: Gather initial details about the murder case
+-- Step 1: Gather initial details about the murder case
 -- Find the crime scene report for the murder on January 15, 2018 in SQL City
 
 SELECT *
@@ -7,7 +7,7 @@ WHERE type = 'murder'
   AND date = '2018-01-15'
   AND city = 'SQL City';
 
--- 📝 Step 2: Find the witness names and their addresses mentioned in the report
+-- Step 2: Find the witness names and their addresses mentioned in the report
 -- (Replace '123' with the actual report_id if needed)
 
 SELECT name, address
