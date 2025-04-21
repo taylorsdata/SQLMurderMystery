40228036246f166d983f772e7e68fddf599f1efb
Tay <taylorhaynes21@gmail.com>
2025-04-21 10:03:34 -0500

    Update 02_find_suspect.sql

--- queries/02_find_suspect.sql
+++ queries/02_find_suspect.sql
@@ -1,1 +1,7 @@
- # Narrowing down based on witness clues
+-- Find people who matched the witness description and were in the area
+SELECT p.name, p.age, p.tattoo, e.event_name
+FROM people p
+JOIN event_checkins e ON p.id = e.person_id
+WHERE p.hair_color = 'red'
+AND p.tattoo = 'snake'
+AND e.date = '2018-01-15';
