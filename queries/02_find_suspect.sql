f6bf61eea1ffd1ebd9cb81bf6fc818bcff87c94b
Tay <taylorhaynes21@gmail.com>
2025-04-21 10:13:30 -0500

    Update 02_find_suspect.sql

--- queries/02_find_suspect.sql
+++ queries/02_find_suspect.sql
@@ -1,7 +1,18 @@
--- Find people who matched the witness description and were in the area
-SELECT p.name, p.age, p.tattoo, e.event_name
-FROM people p
-JOIN event_checkins e ON p.id = e.person_id
-WHERE p.hair_color = 'red'
-AND p.tattoo = 'snake'
-AND e.date = '2018-01-15';
+-- 📝 Step 3: Check interviews of the two witnesses we found earlier
+
+SELECT *
+FROM interview
+WHERE person_id IN (14887, 16371);  -- IDs from previous query
+
+-- 🕵🏽 Based on interviews:
+-- Witness 1 saw the killer at the gym. Killer left in a car from the parking lot.
+-- Witness 2 mentioned they were a member of the "Gold" gym and checked in earlier.
+
+-- 📝 Step 4: Find people who checked into the gym on 2018-01-09
+
+SELECT p.name, p.license_id, p.id AS person_id
+FROM get_fit_now_check_in g
+JOIN get_fit_now_member m ON g.membership_id = m.id
+JOIN person p ON m.person_id = p.id
+WHERE g.check_in_date = '2018-01-09'
+  AND m.membership_status = 'gold';
